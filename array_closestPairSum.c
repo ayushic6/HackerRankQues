f3e7b5d30d3f3f0f4febbc0b92b136f790ac6b3f
Ayushi Chourasia <ayushichourasia06@gmail.com>
2025-04-07 22:14:27 +0530

    Update array_closestPairSum.c

--- array_closestPairSum.c
+++ array_closestPairSum.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <limits.h>
-
 int main() {
     int size;
     scanf("%d", &size);
@@ -12,7 +11,6 @@ int main() {
     
     int closest_sum = INT_MAX;
     int first = 0, second = 0;
-
     for (int i = 0; i < size - 1; i++) {
         for (int j = i + 1; j < size; j++) {
             int current_sum = a[i] + a[j];

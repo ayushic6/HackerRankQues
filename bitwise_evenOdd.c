2d9f9e2b3751bde9b9e221b06e5b5e83bc4e7d98
Ayushi Chourasia <ayushichourasia06@gmail.com>
2025-04-19 21:19:36 +0530

    Update bitwise_evenOdd.c

--- bitwise_evenOdd.c
+++ bitwise_evenOdd.c
@@ -7,17 +7,12 @@ int main() {
     if(num>=-100000 && num<=100000)
     {
         if(num & 1==1)
-        {
             printf("Odd");
-        }
         else
-        {
             printf("Even");
-        }
     }
     else
-    {
         printf("Invalid");
-    }
+    
     return 0;
 }

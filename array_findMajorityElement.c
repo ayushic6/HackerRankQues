465acec3ebfacce2c52d4eb96fe05d3fe4cf8f39
Ayushi Chourasia <ayushichourasia06@gmail.com>
2025-04-19 21:18:10 +0530

    Update array_findMajorityElement.c

--- array_findMajorityElement.c
+++ array_findMajorityElement.c
@@ -5,13 +5,11 @@ int n;
 scanf("%d",&n);
 int majCount = ceil(n/2);
 int arr[n];
-for(int i=0 ; i<=n-1 ; i++){
+for(int i=0 ; i<=n-1 ; i++)
   scanf("%d",&arr[i]);
-}
 int freq[2000001]={0};
-for(int i=0 ; i<=n-1 ; i++){
+for(int i=0 ; i<=n-1 ; i++)
     freq[arr[i]+1000000]++;
-}
 for(int i=0 ; i<=n-1;i++){
   if(freq[arr[i]+1000000]>majCount){
     printf("%d",arr[i]);
